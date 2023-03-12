1b24228a07ba1debcc28a0183ce0c59b1f490ab0
Pete W <115192115+petewrixon@users.noreply.github.com>
2023-03-12 19:51:29 +0000

    Fix input_files naming of list elements.

--- R/setup.R
+++ R/setup.R
@@ -61,6 +61,9 @@ input_files <- rrapply::rrapply(meta$config$input$urls, how = "melt") %>%
   mutate(basename = basename(file.path(src_url))) %>%
   mutate(destfile = file.path(inputDir,basename))
 
+input_files <- setNames(split(input_files, seq(nrow(input_files))),
+         input_files$file_type)
+
 for (i in 1:nrow(input_files)) {
   
   download.file(url = input_files$src_url[[i]], 
