43028da0ca87de56d687a15cd16a32971862432a
Pete Wrixon <peter.wrixon@hmtreasury.gov.uk>
2023-03-13 11:31:46 +0000

    Set overwrite property to pim-run.log

--- R/sink-end-of-code.R
+++ R/sink-end-of-code.R
@@ -3,7 +3,7 @@ sink()
 if (exists("outputDir")) {
   file.copy(run.log.tmp.path, file.path(outputDir,"pim-run.log"))
 } else {
-  file.copy(run.log.tmp.path, file.path(getwd(),"pim-run.log"))
+  file.copy(run.log.tmp.path, file.path(getwd(),"pim-run.log"), overwrite = TRUE)
 }
 
 file.remove(run.log.tmp.path)
